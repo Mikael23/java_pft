0fb42e244cbfbb9edd7e3c63499d0cfafcc5a10b
Mikael23 <mpj2310@gmail.com>
2017-02-12 12:41:04 +0200

    Задания, проделанные на уроке

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -4,8 +4,35 @@ public class MyFirstProgram{
 
     public static void main(String[]args){
 System.out.println("Hello,world");
+Hello("world");
+Hello("dear");
+Hello("Mikle");
+Square s = new Square(5);
+
+System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area()) ;
+
+Rectangle r = new Rectangle(4,6);
+
+System.out.println("Площадь прямоугольника " + r.a +  "и " +r.b + "=" + r.area());
+
+
+
+/*int a = 10;
+int r = a*a;
+        System.out.println("Сторона квадрата со стороной" +" "+ a
+                + " " + "равна" +" " + r);*/
 
 }
+    public static void Hello(String somebody){
+
+        System.out.println("Hello"+ " " + somebody);
+
+    }
+
+    public static int Hello(int a,int b){
+
+        return a*b;
+    }
 
 
 }
